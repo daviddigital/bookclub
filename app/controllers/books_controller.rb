429a5bac7aca5cb3e68bd91c79f5fa862104bbd5
David Watts <davidwatts11@gmail.com>
2021-10-26 14:27:45 +1000

    edit, show, delete actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,54 @@
 class BooksController < ApplicationController
+  before_action :set_authors_and_genres, only: [:new, :edit]
+  before_action :set_book, only: [:edit, :update, :show, :destroy]
+
+  def new
+    @book = Book.new
+  end
+
   def index
     @books = Book.order(title: :asc)
   end
 
   def create
+    # render json: params
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def update
+    @book.update(book_params)
+    redirect_to @book
   end
 
   def edit
   end
 
   def destroy
+    @book.destroy
+    redirect_to books_path
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :publisher, :author_id, :date_published, :publisher, genre_ids: [])
+
+  end
+
+  def set_authors_and_genres
+    @authors = Author.order(:first_name)
+    @genres = Genre.order(:genre)
+  end 
+
+  def set_book
+    @book = Book.find(params[:id])  
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     belongs_to :author
-    has_many :book_genres
+    has_many :book_genres, dependent: :destroy
     has_many :genres, through: :book_genres
 end

2109fbc9993fec4dffe6fb39b855cc0e567e7de2
David Watts <davidwatts11@gmail.com>
2021-11-03 11:29:26 +1000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@ foo = Author.create(first_name: "Test", last_name: "Author")
 bar = Author.create(first_name: "Another", last_name: "Person")
 
 genres = [
-    Genre.create!(name: "Fantasy"),
-    Genre.create!(name: "Sport"),
-    Genre.create!(name: "Romance"),
-    Genre.create!(name: "Sci-Fi"),
-    Genre.create!(name: "Political"),
-    Genre.create!(name: "Other"),
-    Genre.create!(name: "Children")
+    Genre.create!(genre: "Fantasy"),
+    Genre.create!(genre: "Sport"),
+    Genre.create!(genre: "Romance"),
+    Genre.create!(genre: "Sci-Fi"),
+    Genre.create!(genre: "Political"),
+    Genre.create!(genre: "Other"),
+    Genre.create!(genre: "Children")
 ]
 
 Book.create!(title: "Harry Potter", author: jkr, publisher: "Penguin", date_published: "2021-10-26", genres: [genres[0], genres[6]])
